3691dbfb64bf0278809613a087dd5f68479df46f
kelvin901 <wawerukelvin2019@gmail.com>
2023-05-18 21:42:30 +0300

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,37 @@
+/class Dog
+  # setter method
+  def name=(value)
+    @name = value
+  end
+
+  # getter method
+  def name
+    # access the data from the @name instance variable and return it
+    @name
+  end
+end
+
+buddy = Dog.new
+buddy.name = "buddy"
+
+# call the Dog#name method
+puts buddy.name/
+
 class Dog
 
-end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  buddy = Dog.new
+  buddy.name = "buddy"
+  
+  puts buddy.name
+
+
+
